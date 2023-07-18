ae1226e99120dafe682163e6d1066dfc1da55ab2
303B-10\cre <a>
2023-07-18 19:29:19 +0900

    php

---
+++ kadai1/q5.php
@@ -0,0 +1,12 @@
+<!--問題 5（中級）:
+年齢を表す変数 age があります。年齢が 20 歳以上かつ 65 歳以下なら「You are an adult.」、それ以外の場合は「You are not an adult.」と表示する PHP プログラムを作成してください。
+
+<?php
+$age = 66;
+ 
+if($age >=20 && $age <=65) {
+  echo "You are an adult.";
+} else{
+    echo "You are not an adult.";
+}
+?>
